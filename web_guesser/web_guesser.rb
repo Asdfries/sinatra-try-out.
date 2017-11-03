14f5e5b104cf932d3d3f06f5b531279a68b15a68
Aaron Friesen <aaronfriesen1986@gmail.com>
2017-11-03 02:44:26 +0000

    I4: working guessing game with color changes.

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -2,23 +2,40 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 @@number = rand(100)
+@@guess_count = 5
+@@textcolor = "black"
+@@backcolor = "white"
 
 def guess_check(guessed)
   if guessed > @@number
+    @@guess_count -=1
     if guessed - 5 > @@number
-      "WAY TOO HIGH"
+      @@backcolor = "red"
+      @@textcolor = "yellow"
+      "WAY TOO HIGH!!!"
     else
+      @@backcolor = "yellow"
+      @@textcolor = "blue"
       "Too High!!!"
     end
   elsif guessed < @@number && guessed > 0 
+    @@guess_count -=1
     if guessed + 5 < @@number
+      @@backcolor = "red"
+      @@textcolor = "yellow"
       "WAY TOO LOW!!!"
     else
+      @@backcolor = "yellow"
+      @@textcolor = "blue"
        "Too Low!!!"
     end
   elsif guessed == @@number
+    @@guess_count -=1
+    @@backcolor = "green"
+    @@textcolor = "black"
     "You got it right!!!"
   else
+    @@guess_count = 5
     "Pick a number 0-100"
   end
 end
@@ -26,6 +43,8 @@ end
 get '/' do 
   guess = params["guess"].to_i
   message = guess_check(guess)
-  erb :index, :locals => {:number => @@number, :message => message}
+  erb :index, :locals => {:guess => guess, :number => @@number, :message => message, :textcolor => @@textcolor, :backcolor => @@backcolor, :guess_count => @@guess_count}
 end
+
+
 
