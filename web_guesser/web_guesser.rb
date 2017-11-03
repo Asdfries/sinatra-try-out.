fdb5e918ec831a6a91f731843cec0f556bed882a
Aaron Friesen <aaronfriesen1986@gmail.com>
2017-11-03 00:35:07 +0000

    I2: uses an ERB template and HTML

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -1,12 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+@@number = rand(100)
 
-x = rand(101)
+def guess_check(guessed)
+  if guessed > @@number
+    if guessed - 5 > @@number
+      "WAY TOO HIGH"
+    else
+      "Too High!!!"
+    end
+  elsif guessed < @@number && guessed > 0 
+    if guessed + 5 < @@number
+      "WAY TOO LOW!!!"
+    else
+       "Too Low!!!"
+    end
+  elsif guessed == @@number
+    "You got it right!!!"
+  else
+    "Pick a number 0-100"
+  end
+end
 
 get '/' do 
-
-    "THE SECRET NUMBER IS #{x}"
-    
+  guess = params["guess"].to_i
+  message = guess_check(guess)
+  erb :index, :locals => {:number => @@number, :message => message}
 end
 
